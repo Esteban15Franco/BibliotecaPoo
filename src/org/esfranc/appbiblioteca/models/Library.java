469785dbfdd493cbe236b69a19d5655100967b26
Juan_Es <juanesgsw@gmail.com>
2025-12-22 18:29:27 +0100

    feat: crear funciones agregar libros y clientes

--- src/org/esfranc/appbiblioteca/models/Library.java
+++ src/org/esfranc/appbiblioteca/models/Library.java
@@ -3,10 +3,29 @@ package org.esfranc.appbiblioteca.models;
 import java.util.Date;
 
 public class Library {
-    private Book[] books;
-    private Customer[] customers;
+    private Book[] books = new Book[0];
+    private Customer[] customers = new Customer[0];
     private Date date;
     private State state;
+
+
+    public void addBooks(Book book){
+        Book[] auxBook = new Book[books.length +1];
+        for (int i = 0; i < books.length; i++){
+            auxBook[i] = books[i];
+        }
+        auxBook[auxBook.length-1] = book;
+        books = auxBook;
+    }
+
+    public void addCustomer(Customer customer){
+        Customer[] auxCustomer = new Customer[customers.length+1];
+        for (int i = 0; i < customers.length; i++){
+            auxCustomer[i] = customers[i];
+        }
+        auxCustomer[auxCustomer.length-1] = customer;
+        customers = auxCustomer;
+    }
 
 
 }
