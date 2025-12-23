52613447d71540aa78a2fd3bd1383dc84401129b
Juan_Es <juanesgsw@gmail.com>
2025-12-23 20:25:07 +0100

    feat: crea el main

--- src/org/esfranc/appbiblioteca/EjemploBiblioteca.java
+++ src/org/esfranc/appbiblioteca/EjemploBiblioteca.java
@@ -7,9 +7,6 @@ public class EjemploBiblioteca {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        Customer juan = new Customer("Juan Esteban", "Z2786127G");
-        Book newBook = new Book("Cien años de soledad", "Gabriel Garcia Marquez", Gender.HISTORICA);
-
 
     }
 }
